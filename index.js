a30858848973a0125974e11adc53630a711cfdb7
behdad222 <behdad.222@gmail.com>
2019-12-11 20:32:49 +0330

    add tapsell plus functions

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+let TapsellPlusUtils = require("./src/tapsellPlusUtils.js");
 
-import { NativeModules } from 'react-native';
-
-const { RNTapsellPlusSdk } = NativeModules;
-
-export default RNTapsellPlusSdk;
+module.exports = {
+  initialize: TapsellPlusUtils.initialize,
+  setDebugMode: TapsellPlusUtils.setDebugMode,
+  addFacebookTestDevice: TapsellPlusUtils.addFacebookTestDevice,
+  requestRewarded: TapsellPlusUtils.requestRewarded,
+  requestInterstitial: TapsellPlusUtils.requestInterstitial,
+  requestNative: TapsellPlusUtils.requestNative,
+  showAd: TapsellPlusUtils.showAd,
+  nativeAdClicked: TapsellPlusUtils.nativeAdClicked
+};
--- src/tapsell-util.js
+++
@@ -1,7 +0,0 @@
-let TapsellPlus = require("react-native").NativeModules.RNTapsellPlusSdk;
-
-module.exports = {
-  initialize: function(appKey) {
-    TapsellPlus.initialize(appKey);
-  }
-};
